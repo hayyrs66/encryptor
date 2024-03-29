4a6c6c0f0d43b0a6d8a6cf19f0cf174fee02a62a
hayyrs66 <rayrtsx@proton.me>
2024-03-28 19:28:46 -0600

    Add tailwind.config.mjs file

---
+++ tailwind.config.mjs
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {
+      backgroundImage: (theme) => ({
+        "gradient-to-45":
+          "linear-gradient(45deg, #09090b , rgba(255, 255, 255, 0.03))",
+      }),
+      colors: {
+        "link-gray": "hsl(0 0% 98%/0.6)",
+        "link-gray-hover": "hsl(0 0% 98%/0.8)",
+      },
+    },
+  },
+  plugins: [],
+};
