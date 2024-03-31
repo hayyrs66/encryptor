6ea541d3391601315bc747d3eeeab9fa91f31b7a
hayyrs66 <rayrtsx@proton.me>
2024-03-30 23:30:14 -0600

    Update Astro configuration and remove global CSS file

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,10 @@ import icon from "astro-icon";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react(), tailwind(), icon(
+  output: "static",
+  integrations: [react(), tailwind({
+    applyBaseStyles: false,
+  }), icon(
     {
       include:{
         lucide: ["github", "info"],
